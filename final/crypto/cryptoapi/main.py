855d3f74cd18a307b898728050781bb793872947
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2022-12-15 06:38:53 +0000

    Automatically redirect "GET /" to /docs

--- final/crypto/cryptoapi/main.py
+++ final/crypto/cryptoapi/main.py
@@ -1,11 +1,15 @@
 from .crypto import encrypt
 from fastapi import FastAPI, UploadFile
+from fastapi.responses import RedirectResponse
 
 app = FastAPI(
     title="CryptoAPI",
     version="0.0.1",
 )
 
+@app.get("/", include_in_schema=False)
+async def docs_redirect():
+    return RedirectResponse(url="/docs")
 
 @app.post("/crypto/encrypt")
 async def encrypt_file(file: UploadFile):
