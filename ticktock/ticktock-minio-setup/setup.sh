26a4ad2e5d5126d2e62119f9565efad130a9bd80
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2022-12-15 13:00:20 +0000

    Fix MinIO setup script

--- ticktock/ticktock-minio-setup/setup.sh
+++ ticktock/ticktock-minio-setup/setup.sh
@@ -5,7 +5,7 @@ set -exuo pipefail
 mc alias set ticktock "http://${MINIO_HOST}:9000" "${MINIO_ROOT_USER}" "${MINIO_ROOT_PASSWORD}"
 
 mc mb -p "ticktock/${MINIO_BUCKET_AVATARS}"
-mc policy set public "ticktock/${MINIO_BUCKET_AVATARS}"
+mc anonymous set public "ticktock/${MINIO_BUCKET_AVATARS}"
 
 mc mb -p "ticktock/${MINIO_BUCKET_GALLERY}"
 
